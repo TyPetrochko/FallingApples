aa51750aedaa8e083ab187fcb65a6ce586c28e8a
Tyler Petrochko <ty.petrochko@gmail.com>
2015-01-24 15:39:33 -0500

    Finished Shotgun with a raytracing algorithm, added a weaponchanger script, and debugged shotgun.
    TODO: Add better aim-assist, and add a master "HUD" script for health,
    ammo, reticule, etc.

--- Gravity/Assets/Scripts/Weapons/Shotgun.cs
+++ Gravity/Assets/Scripts/Weapons/Shotgun.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 
 public class Shotgun : Photon.MonoBehaviour {
@@ -11,59 +11,109 @@ public class Shotgun : Photon.MonoBehaviour {
 	public float power = 30f;
 	public float damage = 5f;
 
+	public bool debug = true;
+
+
 	public float range = 100f;
 	public float coneRadius = 10f;	
-	public int numberOfRayCasts;
+	public int numberOfRaycastRings = 10;
+	public int raycastDensityPerRing = 4;
+
+
+	private Ray[] rays;
 
 	// Use this for initialization
 	void Start () {
-		if (numberOfRayCasts == null){
-			numberOfRayCasts = (coneRadius*Mathf.PI);
+		if (firepoint==null){
+			firepoint = transform;
 		}
+
+		List<Ray> l = new List<Ray>();
+
+		l.Add(new Ray(firepoint.position, transform.TransformDirection(new Vector3(0, 0, range))));
+		
+		for (int c = 1; c<=numberOfRaycastRings; c++){
+			float ringRadius = (numberOfRaycastRings/c)*coneRadius;
+			int numberOfRaycastsInthisRing = ((int) (2*Mathf.PI))*raycastDensityPerRing;
+			float angleToIncrementBy = (2*Mathf.PI)/numberOfRaycastsInthisRing;
+			float currentAngle = 0;
+			for (int a = 0; a<numberOfRaycastsInthisRing; a++){
+				l.Add(new Ray(firepoint.position, new Vector3(Mathf.Cos(currentAngle)*ringRadius, Mathf.Sin(currentAngle)*ringRadius ,range)));
+				currentAngle += angleToIncrementBy;
+			}
+		}
+
+		rays = l.ToArray();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float radiusDeltaPerRevolution = coneRadius/numberOfRayCasts;
 
-		int angle = 0;
-		int currentRadius = 0;
+		if (debug){
+			// Draw a Straight-ahead raycast
+			Debug.DrawRay(firepoint.position, transform.TransformDirection(new Vector3(0, 0, range)));
 
-		// Draw Raycasts in a spiral shape
-		for (int f = 0; f<numberOfRayCasts; f++){
-			Debug.DrawRay();// FINISH THIS RIGHT HERE
+			for (int c = 1; c<=numberOfRaycastRings; c++){
+				float ringRadius = (numberOfRaycastRings/c)*coneRadius;
+				int numberOfRaycastsInthisRing = ((int) (2*Mathf.PI))*raycastDensityPerRing;
+				float angleToIncrementBy = (2*Mathf.PI)/numberOfRaycastsInthisRing;
+				float currentAngle = 0;
+				for (int a = 0; a<numberOfRaycastsInthisRing; a++){
+					Debug.DrawRay(firepoint.position, transform.TransformDirection(new Vector3(Mathf.Cos(currentAngle)*ringRadius, Mathf.Sin(currentAngle)*ringRadius ,range)), Color.blue);
+					currentAngle += angleToIncrementBy;
+				}
+			}
 		}
 
-		if(Input.GetButtonDown("Fire1")  && photonView.isMine){
+		if(Input.GetButtonDown("Fire1")){
 			// "Kick" back as if firing a large gun
 			
-			GetComponentInParent<Rigidbody>().velocity += -transform.InverseTransformVector(Vector3.forward)*Kickback;
+			GetComponentInParent<Rigidbody>().velocity += -firepoint.forward.normalized*Kickback;
 
+			Fire();
 
 		}
 
-
-
-
-		
-	}
-
-	void OnTriggerStay(Collider c){
-		GameObject obj = c.gameObject;
-		if(Input.GetButtonDown("Fire1")  && photonView.isMine){
-
-			Destroy(this);
-			
-			if (obj.tag == "Player"){
-				
-				// Code for successful hit here!
-				Vector3 blastDirection = transform.InverseTransformVector(Vector3.forward);
-				obj.GetPhotonView().RPC("Nudge", PhotonTargets.AllViaServer, new object[] {blastDirection, power});
-				obj.GetPhotonView().RPC("Damage", PhotonTargets.AllViaServer, new object[] {damage});
-				Debug.Log("HIT!");
+		for (int c = 0; c<rays.Length; c++){
+			RaycastHit hit;
+			if (Physics.Raycast(firepoint.position, firepoint.InverseTransformDirection(rays[c].direction), out hit)){
+				if (hit.collider.tag == "Player"){
+					// Change reticule, then return
+					break;
+				}
 			}
-			PhotonNetwork.Instantiate("Explosion", c.transform.position, Quaternion.identity, 0);
 		}
+
+		//UNDO RETICULE CHANGE (should skip if returned by loop)
+
+	} // END UPDATE
+
+	private void Fire () {
+
+		List<GameObject> p = new List<GameObject>();
+
+		for (int c = 0; c<rays.Length; c++){
+			RaycastHit hit;
+			if (Physics.Raycast(firepoint.position, transform.TransformDirection(rays[c].direction), out hit)){
+				GameObject g = hit.collider.gameObject;
+				if (g.tag == "Player"){
+					if (!p.Contains(g)){
+						p.Add(g);
+					}
+				}
+			}
+		}
+
+		if (p.Count != 0){
+			for (int v = 0; v< p.Count; v++){
+				GameObject d = p[v];
+
+				d.GetPhotonView().RPC("Nudge", PhotonTargets.AllViaServer, new object[] {d.transform.position-firepoint.position, power});
+				d.GetPhotonView().RPC("Damage", PhotonTargets.AllViaServer, new object[] {damage});
+			}
+		}
+
 	}
 
 }
---
+++ Gravity/Assets/Scripts/Weapons/WeaponChanger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponChanger : MonoBehaviour {
+
+	public GameObject [] guns;
+	public Transform gunLocation;
+
+	private GameObject currentGun;
+
+	// Use this for initialization
+	void Start () {
+		if (guns.Length != 0){
+			SetGun(guns[0]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Offset is used since pressing 1 corresponds
+		// to guns[0]
+		for(int a = 1; a<(guns.Length+1);a++) {
+			if(Input.GetKeyDown(""+a)){
+				SetGun(guns[a-1]);
+			}
+		}
+	}
+
+	void SetGun (GameObject gun){
+		if (gunLocation.childCount==1){
+			Destroy(gunLocation.GetChild(0).gameObject);
+		}
+		GameObject s = (GameObject) Instantiate(gun, gunLocation.position, gunLocation.rotation);
+		s.transform.parent = gunLocation;
+
+		foreach(MonoBehaviour m in s.GetComponents<MonoBehaviour>()){
+			if(m.enabled == false){
+				m.enabled=true;
+			}
+			
+		}
+		
+	}
+}
